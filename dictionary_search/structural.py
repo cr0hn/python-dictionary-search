f2d2338e9970cccbb094118634ca66add0e34415
César Gallego Rodríguez <gallego.cesar@gmail.com>
2023-07-26 18:59:45 +0200

    feat: nest function

--- dictionary_search/structural.py
+++ dictionary_search/structural.py
@@ -1,11 +1,13 @@
-from typing import Iterable
-from functools import singledispatch
+from typing import Iterable, List, Any, Tuple
+from functools import singledispatch, reduce
+from itertools import groupby
 
 
 def __next_current(current: str, n: str):
     if current != "":
         return f"{current}.{n}"
     return n
+
 
 @singledispatch
 def flat(value, current: str = "") -> Iterable[tuple]:
@@ -26,4 +28,48 @@ def _(l: list, current: str = "") -> Iterable[tuple]:
         yield from flat(v, nc)
 
 
-__all__ = ["flat"]
+def __part_type(part: str) -> str | int:
+    if part.startswith("[") and part.endswith("]"):
+        return int(part[1:-1])
+    return part
+
+
+def __nest_recursion(data, part_value):
+    parts, value = part_value
+    if isinstance(parts, list) and len(parts) == 1:
+        if isinstance(parts[0], int) and len(data) <= parts[0]:
+            data.extend([None] * (parts[0] - len(data) + 1))
+        data[parts[0]] = value
+    elif isinstance(parts, list) and isinstance(parts[0], str):
+        if parts[0] not in data:
+            data[parts[0]] = {}
+        data[parts[0]] = __nest_recursion(data[parts[0]], (parts[1:], value))
+    elif isinstance(parts[0], int):
+        if len(data) <= parts[0]:
+            data.extend([None] * (parts[0] - len(data) + 1))
+        data[parts[0]] = __nest_recursion(data[parts[0]], (parts[1:], value))
+    else:
+        raise NotImplementedError
+    return data
+
+
+def nest(items: List[Tuple[str, Any]]): 
+    keys = [x for x, _ in items]
+    level_parts = [[__part_type(p) for p in x.split(".")] for x in keys]
+    values = [x for _, x in items]
+
+    # type check
+    types = set(map(type, [x[0] for x in level_parts]))
+    if len(types) > 1:
+        raise ValueError("Cannot nest mixed types")
+    level_type = types.pop()
+    type_values = list(zip(level_parts, values))
+
+    if level_type == str:
+        return reduce(__nest_recursion, type_values, {})
+    elif level_type == int:
+        return reduce(__nest_recursion, type_values, [])
+    raise NotImplementedError
+
+
+__all__ = ["flat", "nest"]
---
+++ tests/structural/test_nest.py
@@ -0,0 +1,35 @@
+import pytest
+
+from dictionary_search import nest
+
+
+def test_mixing_types():
+    with pytest.raises(ValueError):
+        d = [("a", 1), ("[0]", 2)]
+        nest(d)
+
+
+def test_simple_nest():
+    base = {"a": 1, "b": 2}
+    d = list({"a": 1, "b": 2}.items())
+    assert nest(d) == base 
+
+
+def test_nest_prop():
+    d = list({"a.b": 1, "a.c": 2}.items())
+    assert nest(d) == {"a": {"b": 1, "c": 2}}
+
+
+def test_mixed_nesting():
+    d = list({"a.b": 1, "a.c": 2, "d": 3}.items())
+    assert nest(d) == {"a": {"b": 1, "c": 2}, "d": 3}
+
+
+def test_list_nesting():
+    d = [("a.b", 1), ("a.c", 2), ("d", 3)]
+    assert nest(d) == {"a": {"b": 1, "c": 2}, "d": 3}
+
+
+def test_index_nesting():
+    d = [("[0]", 1), ("[1]", 2), ("[2]", 3)]
+    assert nest(d) == [1, 2, 3]
